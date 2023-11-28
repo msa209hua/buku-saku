8d1690d0a27331eaba8c5d85bc28a668bc44ec63
Mon3tr <naokikaindra@gmail.com>
2023-11-28 11:51:22 +0700

    add boostrap

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
       body {
         font-family: arial;
@@ -147,6 +148,7 @@
     </style>
 </head>
 <body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <div class="form-container">
 	<p class="title">Login <br> <span style="font-size: 18px;">E - Buku Saku</span></p>
 	<form class="form" action="proses-login.php" method="POST">
