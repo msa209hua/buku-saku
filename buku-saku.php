a387d6404c9e1a71b237b37d38b35df5ab2647b3
Mon3tr <naokikaindra@gmail.com>
2023-12-05 08:14:46 +0700

    new

--- buku-saku.php
+++ buku-saku.php
@@ -1,54 +1,94 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>e - Buku Saku</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-      .nav {
-        list-style-type: none;
-        height: 80px;
-        width: 100%;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: #181c24;;
-      }
-      h1 {
-        color: white;
-        background-color: #282C34;
-        padding: 10px 20px 35px 20px;
-      }
 
-</style>
-  </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>e - Buku Saku</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <style>
+    .nav {
+      list-style-type: none;
+      height: 80px;
+      width: 100%;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background-color: #181c24;
+      ;
+    }
 
-    <div class="nav">
-      <h1>e-Buku Saku</h1>
-    </div>
+    h1 {
+      color: white;
+      background-color: #282C34;
+      padding: 10px 20px 35px 20px;
+    }
 
-    <div class="identitas">
-      <table class="table table-secondary table-striped table-responsive-md">
-        <tr>
-          <td>Nama Lengkap</td>
-          <td>: </td>
-        </tr>
-        <tr>
-          <td>NIS</td>
-          <td>: </td>
-        </tr>
-        <tr>
-          <td>Kelas</td>
-          <td>: </td>
-        </tr>
-        <tr>
-          <td>Poin</td>
-          <td>: </td>
-        </tr>
-      </table>
-    </div>
-  </body>
+    .judul-1 {
+      background-color: #d7d8da;
+      text-align: center;
+      font-weight: 900;
+      font-size: 30px;
+      margin-top: 55px;
+      border-top: 1px solid #b5b6b7;
+      border-bottom: 1px solid #b5b6b7;
+    }
+  </style>
+</head>
+
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+
+  <div class="nav">
+    <h1>e-Buku Saku</h1>
+  </div>
+
+  <div class="identitas">
+    <table class="table table-secondary table-striped table-responsive-md">
+      <tr>
+        <td><b>Nama Lengkap</b></td>
+        <td><b>:
+            <!-- dari databes -->
+          </b></td>
+      </tr>
+      <tr>
+        <td><b>NIS</b></td>
+        <td><b>:
+            <!-- dari databes -->
+          </b></td>
+      </tr>
+      <tr>
+        <td><b>Kelas</b></td>
+        <td><b>:
+            <!-- dari databes -->
+          </b></td>
+      </tr>
+      <tr>
+        <td><b>Total Poin</b></td>
+        <td><b>:
+            <!-- dari databes -->
+          </b></td>
+      </tr>
+    </table>
+  </div>
+
+  <div class="catatan">
+    <p class="judul-1">Catatan Kasus</p>
+    <table class="table table-striped table-secondary">
+      <tr>
+        <td>NO</td>
+        <td>HARI/TANGGAL</td>
+        <td>PELANGGARAN & KEBAIKAN</td>
+        <td>POIN</td>
+      </tr>
+      <tr>
+        
+      </tr>
+    </table>
+  </div>
+</body>
+
 </html>
