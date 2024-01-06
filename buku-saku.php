35845e10201cd136fe13777c2ff972815e8dfbae
Bintang <naokikaindra@gmail.com>
2024-01-07 06:01:38 +0700

    buku saku clear

--- buku-saku.php
+++ buku-saku.php
@@ -66,6 +66,37 @@
       margin-top: -16px;
     }
 
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      outline: none;
+      cursor: pointer;
+      width: 150px;
+      height: 50px;
+      background-image: linear-gradient(to top, #D8D9DB 0%, #fff 80%, #FDFDFD 100%);
+      border-radius: 30px;
+      border: 1px solid #8F9092;
+      transition: all 0.2s ease;
+      font-family: "Source Sans Pro", sans-serif;
+      font-size: 14px;
+      font-weight: 600;
+      color: #606060;
+      text-shadow: 0 1px #fff;
+    }
+
+    button:hover {
+      box-shadow: 0 4px 3px 1px #FCFCFC, 0 6px 8px #D6D7D9, 0 -4px 4px #CECFD1, 0 -6px 4px #FEFEFE, inset 0 0 3px 3px #CECFD1;
+    }
+
+    button:active {
+      box-shadow: 0 4px 3px 1px #FCFCFC, 0 6px 8px #D6D7D9, 0 -4px 4px #CECFD1, 0 -6px 4px #FEFEFE, inset 0 0 5px 3px #999, inset 0 0 30px #aaa;
+    }
+
+    button:focus {
+      box-shadow: 0 4px 3px 1px #FCFCFC, 0 6px 8px #D6D7D9, 0 -4px 4px #CECFD1, 0 -6px 4px #FEFEFE, inset 0 0 5px 3px #999, inset 0 0 30px #aaa;
+    }
+
     @media screen and (max-width: 600px) {
       h1 {
         padding-top: 23px;
@@ -77,6 +108,11 @@
 
       .table-2, .table-3, .table-4 {
         font-size: 12px;
+      }
+
+      .pedoman img {
+        width: 400px;
+        height: 600px;
       }
     }
   </style>
@@ -313,6 +349,7 @@
   </div>
 
   <div class="pedoman" style="text-align: center;">
+  <p class="judul">PEDOMAN</p>
     <img src="img/pedoman_1.png" alt=""><br>
     <img src="img/pedoman_2.png" alt=""><br>
     <img src="img/pedoman_3.png" alt=""><br>
@@ -336,8 +373,14 @@
     <img src="img/pedoman_21.png" alt=""><br>
     <img src="img/pedoman_22.png" alt=""><br>
     <img src="img/pedoman_23.png" alt=""><br>
-    <img src="img/pedoman_24.png" alt=""><br>
   </div>
+
+<?php
+  echo "<form action='buku-saku.php?id=$id' method='post'>"
+  ?>
+  <button class="button" style="margin-left: 33%; margin-top: 40px; margin-bottom: -40px;">Kembali Ke Atas</button>
+
+</form>
 
   <div class="footer">
     <p>&copy;TEFA RPL</p>
