6906307f9831801faade4f919aacd9b4c7bcf65f
Mon3tr <naokikaindra@gmail.com>
2024-02-05 09:27:20 +0700

    apa weh

--- admin-buksak.php
+++ admin-buksak.php
@@ -152,11 +152,11 @@
 
 <body>
 <?php
-  // session_start();
+  session_start();
 
-  // if (!isset($_SESSION['nis'])) {
-  //   header('Location: index.php');
-  // }
+  if (!isset($_SESSION['id_masuk'])) {
+    header('Location: index.php');
+  }
 ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <?php
