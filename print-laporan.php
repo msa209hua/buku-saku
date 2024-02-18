5241196f67f6fe89e9ff5533ffb33178eaf7b803
Tanszah <117809160+ArtoriaPen02@users.noreply.github.com>
2024-02-18 21:11:09 +0700

    exe;

--- print-laporan.php
+++ print-laporan.php
@@ -1,3 +1,7 @@
+<?php
+  include "koneksi.php";
+  $sql = mysqli_query($conn, "SELECT * FROM tb_pelanggaran")
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +10,37 @@
     <title>Document</title>
 </head>
 <body>
+<?php
     
+        
+      header("Content-type: application/vnd-ms-excel");
+      header("Content-Disposition: attachment; filename=Data Kasus.xls");
+    
+    ?>
+<table>
+      <tr>
+        <th>Tanggal</th>
+        <th>NIS</th>
+        <th>Nama</th>
+        <th>Kelas</th>
+        <th>Pelanggaran</th>
+        <th>Poin</th>
+        <th>Keterangan</th>
+      </tr>
+      <?php foreach ($sql as $row) : ?>
+        <tr>
+          <td><?= $row["tanggal"]; ?></td>
+          <td><?= $row["nis"]; ?></td>
+          <td><?= $row["nama_siswa"] ?></td>
+          <td><?= $row["kelas"] ?></td>
+          <td><?= $row["pelanggaran"]; ?></td>
+          <td>-<?= $row["poin_minus"] ?></td>
+          <td><?= $row["keterangan"] ?></td>
+        </tr>
+        </tr>
+      <?php
+      endforeach;
+      ?>
+    </table>
 </body>
 </html>
