2cfb168ef7da571d94ba8dbcf070cceec6013eb9
Mon3tr <naokikaindra@gmail.com>
2024-03-05 14:24:33 +0700

    apdet

--- S.U.P.E.R/proses-kasus.php
+++ S.U.P.E.R/proses-kasus.php
@@ -268,7 +268,7 @@ li a {
           <td><?=$row["pelanggaran"], $row["kebaikan"]; ?></td>
           <td><?=$row["ket_poin"], $row["poin_minus"], $row["poin_plus"]; ?></td>
           <td><?=$row["keterangan"]; ?></td>
-          <td><img src="../image/<?=$row["gambar"]; ?>" width="100" height="100" alt="foto bukti"></td>
+          <td><img src="../image/<?=$row["gambar"]; ?>" width="100" height="100" alt='foto tidak ada' title="Foto Bukti"></td>
           </tr>
           <?php endforeach; ?>
       </table>
--- buku-saku.php
+++ buku-saku.php
@@ -259,8 +259,9 @@
             <td>" . $row["tanggal"] . "</td>
             <td>" . $row["pelanggaran"], $row["kebaikan"] . "</td>
             <td>" . $row["keterangan"] . "</td>
-            <td><img src='image/".$row['gambar']."' width='100' height='100' alt='foto bukti'></td>
+            <td><img src='image/".$row['gambar']."' width='100' height='100' alt='foto tidak ada' title='Foto Bukti'></td>
             <td>" .$row["ket_poin"], $row["poin_minus"], $row["poin_plus"] . "</td>
+            <td>".$row['gambar']."</td>
           <tr/>
           ";
         $nomor++;
