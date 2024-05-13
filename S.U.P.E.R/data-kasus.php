372d94069f000c29c50766a08d12b21a823bbfd6
Mon3tr <naokikaindra@gmail.com>
2024-05-13 09:14:37 +0700

    repair title data kasus

--- S.U.P.E.R/data-kasus.php
+++ S.U.P.E.R/data-kasus.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Data Kasus</title>
   <link rel="stylesheet" href="styleBar.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/
