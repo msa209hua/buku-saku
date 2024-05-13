bd298750d16d54412688055b7821fa9777cba5fc
Mon3tr <naokikaindra@gmail.com>
2024-05-13 09:19:08 +0700

    repair credit title

--- High-Access/credit-buksak.php
+++ High-Access/credit-buksak.php
@@ -6,6 +6,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Credit</title>
   <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
   <link rel="stylesheet" href="styleBar.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
